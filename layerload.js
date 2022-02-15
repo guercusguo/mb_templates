6b7bbc5a2ab7ea84a24498d56f4592636a4a3f5e
guercusguo <dicaguido@gmail.com>
2022-02-15 14:27:14 +0100

    added sun elevation features

--- layerload.js
+++ layerload.js
@@ -1,4 +1,3 @@
-
 //Load of GeoJSON source
 var geojson;
 function addSource() {
@@ -14,7 +13,7 @@ function addSource() {
 	map.addSource('mapbox-dem', {
 		'type': 'raster-dem',
 		'url': 'mapbox://mapbox.mapbox-terrain-dem-v2',
-		'tileSize': 256,
+		'tileSize': 512,
 		'maxzoom': 14
 	});
 }
@@ -94,9 +93,23 @@ map.addLayer({
 	'id': 'sky',
 	'type': 'sky',
 	'paint': {
+		'sky-opacity': [
+			'interpolate',
+			['linear'],
+			['zoom'],
+			0,
+			0,
+			5,
+			0.3,
+			8,
+			1
+		],
+		// set up the sky layer for atmospheric scattering
 		'sky-type': 'atmosphere',
-		'sky-atmosphere-sun': [0.0, 0.0],
-		'sky-atmosphere-sun-intensity': 15
+		// explicitly set the position of the sun rather than allowing the sun to be attached to the main light source
+		'sky-atmosphere-sun': getSunPosition(),
+		// set the intensity of the sun as a light source (0-100 with higher values corresponding to brighter skies)
+		'sky-atmosphere-sun-intensity': 5
 	}
 });
 // 3D properties of terrain source
@@ -129,5 +142,37 @@ function addMarkers () {
 		});
 	});
 }
+// Get list of SunCalc's default sun positions
+// for the current time and location
+const sunPositions = SunCalc.getTimes(
+	Date.now(),
+	map.getCenter().lat,
+	map.getCenter().lng
+);
+// set up event listeners for the buttons to update
+// the position of the sun for times of the day
+const sunTimeLabels = document.querySelectorAll(
+	'#sun input',
+	'#getlocal'
+);
+for (const label of sunTimeLabels) {
+	label.addEventListener('click', () => {
+		const sunPos =
+		label.id === 'getlocal'
+		? getSunPosition(new Date())
+		: getSunPosition(sunPositions[label.id]);
+		updateSunPosition(sunPos);
+	});
+}
 
-</script>
+function getSunPosition(date) {
+	const center = map.getCenter();
+	const sunPos = SunCalc.getPosition(
+		date || Date.now(),
+		center.lat,
+		center.lng
+	);
+	const sunAzimuth = 180 + (sunPos.azimuth * 180) / Math.PI;
+	const sunAltitude = 90 - (sunPos.altitude * 180) / Math.PI;
+	return [sunAzimuth, sunAltitude];
+}
